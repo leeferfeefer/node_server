6c08b33e93a0a8e7166ead807c5b21734c9575b4
dxf2ci5 <dxf2ci5>
2020-04-20 12:19:54 -0400

    Added logging

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+var logger = require('morgan');
 const thingyRoute = require('./routes/thingy.js');
 
 const app = express();
 const port = 3000;
 
+// dev = :method :url :status :response-time ms - :res[content-length]
+
+app.use(logger('dev'));
 app.use('/thingy', thingyRoute);
 app.get('/', (req, res) => res.send('Hello World!'));
 
