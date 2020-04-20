057bd0e4add404403e5e3d840cb22e9372448324
dxf2ci5 <dxf2ci5>
2020-04-20 12:10:58 -0400

    Added route

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const thingyRoute = require('./routes/thingy.js');
+
 const app = express();
 const port = 3000;
 
+app.use('/thingy', thingyRoute);
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
